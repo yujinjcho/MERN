23852e4bab5183afefd8fef34f35718a244aad2f
Yujin <yujinjcho@gmail.com>
2016-10-13 19:56:35 -0700

    5.1

--- webapp.js
+++ webapp.js
@@ -1,8 +1,18 @@
 var express = require('express');
 
 var app = express();
-
 app.use(express.static('static'));
+
+var bugData = [
+  {id: 1, priority:"P1", status:"Open", owner:"Ravan", title:"App crashes on open"},
+  {id: 2, priority:"P2", status:"New", owner:"Eddie", title:"Misaligned border on panel"}
+];
+
+app.get('/api/bugs', function(req, res) {
+  res.status(200).send(JSON.stringify(bugData));
+})
+
+
 
 var server = app.listen(3000, function(){
   var port = server.address().port;
